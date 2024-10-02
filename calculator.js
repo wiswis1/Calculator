ed2496806f1445886e3afc546213f25977c87d41
wiswis1 <elijahwidom6@gmail.com>
2024-10-01 19:43:44 -0500

    Create js file and add 'add, subtract,multiply and divide' functions, add operate function that calls one of the four functions depending on operand chosen

---
+++ calculator.js
@@ -0,0 +1,39 @@
+var num1;
+var operator;
+var num2;
+
+var result;//just in case
+
+
+function operate(num1, operator, num2){
+        switch(operator){
+            case('+'):
+                add(num1, num2);
+            case('-'):
+                subtract(num1,num2);
+            case('*'):
+                multiply(num1, num2);
+            case('/'):
+                divide(num1, num2);
+        }
+        
+}
+
+
+
+
+function add(num1, num2){
+
+}
+
+function subtract(num1, num2){
+
+}
+
+function multiply(num1, num2){
+
+}
+
+function divide(num1, num2){
+
+}
